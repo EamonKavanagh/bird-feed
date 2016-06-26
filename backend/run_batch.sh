7bc0da580dbfdc676e04873903fc2277eaebf8d2
Eamon Kavanagh <kavanagh.c.eamon@gmail.com>
2016-06-26 16:29:17 -0400

    Fixed Cassandra schema, fixed version on batch script

--- backend/run_batch.sh
+++ backend/run_batch.sh
@@ -2,6 +2,6 @@
 
 echo "Running Spark job"
 
-$SPARK_HOME/bin/spark-submit --master spark://172.31.0.46:7077 --class Batch target/bird-feed-1.0-SNAPSHOT-jar-with-dependencies.jar
+$SPARK_HOME/bin/spark-submit --master spark://172.31.0.46:7077 --class Batch target/bird-feed-1.0-jar-with-dependencies.jar
 
 echo "Finished Spark job"
